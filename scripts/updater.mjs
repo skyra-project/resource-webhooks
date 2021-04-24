00701743455a20a7b2f71d14481afb769a73627e
Jeroen Claassens <support@favware.tech>
2021-04-24 22:41:19 +0200

    build: add updater script

---
+++ scripts/updater.mjs
@@ -0,0 +1,31 @@
+import { fetch, FetchMethods, FetchResultTypes } from '@sapphire/fetch';
+import { stripIndents } from 'common-tags';
+
+const content = stripIndents`
+content
+`;
+
+const messageID = '';
+const webhookURL = process.env.WEBHOOK_URL ?? '';
+
+try {
+	const result = await fetch(`${webhookURL}/messages/${messageID}`, {
+		method: FetchMethods.Patch,
+		body: JSON.stringify({
+			content,
+			allowed_mentions: {
+				users: [],
+				roles: []
+			}
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}, FetchResultTypes.Result);
+
+	console.log(`${result.status} - ${result.statusText}`);
+} catch (error) {
+	const errorBody = await error.json();
+	console.error(errorBody);
+	console.error(`${error.status} - ${error.statusText}`);
+}
